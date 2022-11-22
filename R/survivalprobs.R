fdfb992d8b7dfce17e1056cce05d1a6535d07ee1
Ariana <arianahaidari@gmail.com>
2022-11-22 18:50:03 -0500

    add vignettes

---
+++ R/survivalprobs.R
@@ -0,0 +1,15 @@
+#'survival.probs
+#'
+#'Gets a vector of survival probabilities for each time point
+#'
+#'@param x vector of times and vector of events
+#'
+#'@return vector of survival probabilities
+#'
+#'@examples
+#'time = c(3,2,5,4.1)
+#'event = c(1,0,0,1,1)
+#'survival.probs(time, event)
+#'
+#'@export
+#'
