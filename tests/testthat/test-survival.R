cf96b83aca9b49a2a0dc3205dc97a817c325e7c5
Ariana <arianahaidari@gmail.com>
2022-11-22 23:54:42 -0500

    new tests

--- tests/testthat/test-survival.R
+++ tests/testthat/test-survival.R
@@ -1,4 +1,8 @@
 test_that("survival.probs works", {
   expect_equal(survival.probs(time = c(1,2,3,4,5), event = c(0,1,1,1,1)),
                c(0.75, 0.5, 0.25, 0.0))
+  time2 = c(1:6000)
+  event2 = sample(c(0,1), replace=TRUE, size=6000)
+  expect_equal(survival.probs(time2, event2),
+               summary(survival::survfit(survival::Surv(time2, event2)~1))$surv)
 })
